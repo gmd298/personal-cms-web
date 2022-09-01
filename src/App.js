5668167f86a7f1a9d61c9d4769f00b192365f101
Griffin <gmd298@gmail.com>
2022-09-01 13:44:53 -0400

    deleted all the create-react-app stuff i dont think i need, added MUI, and started building Nav component... which is still broken

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Nav/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
