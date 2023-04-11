f3369421ed45cdfb7ba4738f536254d09d20f905
Griffin <gmd298@gmail.com>
2023-04-11 14:01:23 -0400

    I have attempted to add some changes to web app and portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Portfolio() {
+const Portfolio = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const response = await axios.get('/posts');
+      setImages(response.data);
+    };
+    fetchImages();
+  }, []);
+
   return (
-    <div>Portfolio</div>
-  )
-}
+    <div className="portfolio">
+      <h1>My Portfolio</h1>
+      <div className="grid">
+        {images.map((image) => (
+          <div className="grid-item" key={image._id}>
+            <img src={image.url} alt={image.title} />
+            <div className="overlay">
+              <h3>{image.title}</h3>
+              <p>{image.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
